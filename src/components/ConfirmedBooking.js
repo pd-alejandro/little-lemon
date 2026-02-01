dc317d9ad0e050df0ec161b07081e2882419cf2e
Alejandro Perez <alejandro.perez.diaz@protonmail.com>
2026-02-01 01:37:03 +0100

    ConfirmedBooking styles updated

--- src/components/ConfirmedBooking.js
+++ src/components/ConfirmedBooking.js
@@ -1,7 +1,13 @@
 const ConfirmedBooking = () => {
-    return (<>
-    <h2>Booking confirmed</h2>
-    </>);
-}
+  return (
+    <section>
+      <div className="container" style={styles}>
+        <h2>Booking confirmed</h2>
+      </div>
+    </section>
+  );
+};
+
+const styles = {display: "grid", gridTemplateColumns: "auto", justifyContent: "center", margin: "4rem 0"}
 
 export default ConfirmedBooking;
